a96bbee289e3f62534c34f59177dbb0c25304779
unknown <dawaadcw@gmail.com>
2023-04-28 15:41:05 +0200

    Create random key function

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+from numpy import matrix
+
+import hill
+from utils import are_coprime
+
 if __name__ == '__main__':
+
+    alphabet_len = 26
+    key_len = 4
+
+    # Generate non coprime integers
+    not_coprime = [x for x in range(alphabet_len) if are_coprime(x, alphabet_len)]
+
+    # Generate the key
+    key = hill.random_key(4, not_coprime)
+    print(key)
+
     pass
