0fde44155b3ab84188b9b23a28c3434b1b31e64b
Adrian Ćwiąkała <dawaadcw@gmail.com>
2023-04-30 16:54:47 +0200

    crack_cipher function, work in progress

---
+++ crack_cipher.py
@@ -0,0 +1,83 @@
+import numpy as np
+
+from ngram import Ngram_score as ns
+from hill_key import random_key, invert_key, randomize_key
+from hill_encrypt import encrypt
+from time import time
+import random
+
+
+def shotgun_hillclimbing(text: str, key_len: int, alphabet: str, t_limit: int = 60 * 5, j_max: int = 6000,
+                         freqs: list[float] | None = None, buffer_len: int = 7):
+    scorer = ns('./english_bigrams.txt')
+
+    alphabet_len = len(alphabet)
+    key_old = random_key(key_len=key_len, alphabet_len=alphabet_len)
+    value_old = scorer.score(encrypt(text, key_old, alphabet, freqs))
+
+    best_results = []
+    t0, itr, j = time(), 0, 0
+
+    wordlen = len(text)
+
+    while time() - t0 < t_limit:
+        perc = (1 - ((time() - t0) / t_limit)) ** 4
+
+        key_new = randomize_key(key_old, perc, alphabet_len)
+        decoded_new = encrypt(text, key_new, alphabet, freqs)
+        value_new = scorer.score(decoded_new)
+
+        if value_new > value_old:
+            print(f"decoded: {decoded_new[:25]}, value: {value_new}, perc = {perc}")
+
+            if value_new / wordlen > -1:
+                print(f'BEST: {decoded_new}')
+                break
+            key_old, value_old = key_new, value_new
+            j = 0
+
+        if j > j_max:
+            best_results.append((value_old, key_old))
+            if len(best_results) >= buffer_len:
+                best_results.sort(reverse=True, key=lambda t: t[0])
+
+                if best_results[0][0] > -3:
+                    break
+
+                # if best_results[0][1] == best_results[1][1] == best_results[2][1]:
+                #     break
+                first_matrix = best_results[0][1]
+                all_same = True
+                for entry in best_results[1:]:
+                    mat = entry[1]
+                    if not np.array_equal(first_matrix, mat):
+                        all_same = False
+                        break
+
+                if all_same:
+                    break
+
+                if random.random() < 0.8:
+                    value_old, key_old = random.choice(best_results[:buffer_len])
+                else:
+                    key_old = random_key(key_len=key_len, alphabet_len=alphabet_len)
+                    value_old = scorer.score(encrypt(text, key_old, alphabet, freqs))
+
+            else:
+                key_old = random_key(key_len=key_len, alphabet_len=alphabet_len)
+                value_old = scorer.score(encrypt(text, key_old, alphabet, freqs))
+        itr += 1
+        j += 1
+    if len(best_results) > 0:
+        best_results.sort(reverse=True)
+        value_old, key_old = best_results[0]
+
+        print(f'{buffer_len} best results:')
+        for x in best_results[:buffer_len]:
+            print(x)
+
+    print(f'elapsed time is {time() - t0} sec, used {itr} iterations')
+    # print(decode_dict(text, key_old), value_old)
+    print(encrypt(text, key_old, alphabet, freqs), value_old)
+
+    return invert_key(key_old, alphabet_len)
--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from string import ascii_uppercase as alphabet
 import pandas as pd
 
 from hill_encrypt import encrypt, decrypt
-import hill_key
+from hill_key import random_key, randomize_key, swap_rows, invert_key
+from crack_cipher import shotgun_hillclimbing
 
 
 def encrypt_test():
@@ -14,36 +15,71 @@ def encrypt_test():
     letter_data = pd.read_csv("./english_letters.csv")
     freqs = letter_data['frequency'].tolist()
 
-    key = hill_key.random_key(5, len(alphabet))
+    key = random_key(5, len(alphabet))
 
     encrypted = encrypt(text, key, alphabet, freqs=freqs)
     print(f"Encrypted: {encrypted}")
 
     decrypted = decrypt(encrypted, key, alphabet, freqs=freqs)
     print(f"Decrypted: {decrypted}")
+    pass
 
 
 def randomize_key_test():
     alphabet_len = len(alphabet)
     key_l = 3
-    key = hill_key.random_key(key_l, alphabet_len)
+    key = random_key(key_l, alphabet_len)
     print(f"Key: \n{key}")
 
-    changed_key = hill_key.randomize_key(key, percentage=0.5, alphabet_len=alphabet_len)
+    changed_key = randomize_key(key, percentage=0.5, alphabet_len=alphabet_len)
     print(f"Changed key: \n{changed_key}")
 
 
 def swap_rows_test():
     alphabet_len = len(alphabet)
     key_l = 3
-    key = hill_key.random_key(key_l, alphabet_len)
+    key = random_key(key_l, alphabet_len)
     print(f"Key: \n{key}")
 
-    changed_key = hill_key.swap_rows(key)
+    changed_key = swap_rows(key)
     print(f"Changed key: \n{changed_key}")
 
 
+def crack_test():
+    key_l = 3
+    alphabet_len = len(alphabet)
+    text = 'Since different users might have different needs and different assumptions, the NumPy developers' \
+           'refused to guess and instead decided to raise a ValueError whenever one tries to evaluate an array' \
+           'in Boolean context. Applying and to two numpy arrays causes the two arrays to be evaluated in Boolean context'
+
+    letter_data = pd.read_csv("./english_letters.csv")
+    freqs = letter_data['frequency'].tolist()
+
+    key = random_key(key_l, alphabet_len)
+    encrypted = encrypt(text, key, alphabet, freqs)
+
+    cracked_key = shotgun_hillclimbing(encrypted, key_l, alphabet, freqs=freqs)
+    cracked_text = decrypt(encrypted, cracked_key, alphabet, freqs)
+
+    pass
+
+
 if __name__ == '__main__':
-    swap_rows_test()
+    # swap_rows_test()
+    # crack_test()
+
+    # key_l = 3
+    # alphabet_len = len(alphabet)
+    # text = 'Attach files by dragging & dropping, selecting or pasting them.'
+    #
+    # letter_data = pd.read_csv("./english_letters.csv")
+    # freqs = letter_data['frequency'].tolist()
+    #
+    # key = random_key(key_l, alphabet_len)
+    # encrypted = encrypt(text, key, alphabet, freqs)
+    # inv_key = invert_key(key, alphabet_len)
+    # decrypted = encrypt(text, inv_key, alphabet, freqs)
+
+    crack_test()
 
     pass
