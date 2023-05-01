33fc7047f15d5be2d80a0eac494dc26d5ec220d3
Przemek <rysiulaw@gmail.com>
2023-05-01 13:09:02 +0200

    determinant_test

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 from numpy import matrix, array
 from string import ascii_uppercase as alphabet
 import pandas as pd
-
+import numpy as np
 from hill_encrypt import encrypt, decrypt
 from hill_key import random_key, randomize_key, swap_rows, invert_key
 from crack_cipher import shotgun_hillclimbing
@@ -48,21 +48,63 @@ def swap_rows_test():
 def crack_test():
     key_l = 3
     alphabet_len = len(alphabet)
-    text = 'Since different users might have different needs and different assumptions, the NumPy developers' \
-           'refused to guess and instead decided to raise a ValueError whenever one tries to evaluate an array' \
-           'in Boolean context. Applying and to two numpy arrays causes the two arrays to be evaluated in Boolean context'
-
+    text = 'This book is about testing, experimenting, ' \
+           'and playing with language.It is a handbook of ' \
+           'tools and techniques for taking words apart and putting them back together again in ways that ' \
+           'I hope are meaningful and legitimate ( or even illegitimate).This book is about peeling back ' \
+           'layers in search of the language-making energy of the human spirit.It is about the gaps in ' \
+           'meaning that we urgently need to notice and name—the places where our dreams and ideals are ' \
+           'no longer fulfilled by a society that has become fast-paced and hyper-commercialized.'
     letter_data = pd.read_csv("./english_letters.csv")
     freqs = letter_data['frequency'].tolist()
 
     key = random_key(key_l, alphabet_len)
     encrypted = encrypt(text, key, alphabet, freqs)
 
-    cracked_key = shotgun_hillclimbing(encrypted, key_l, alphabet, freqs=freqs)
+    cracked_key = shotgun_hillclimbing(encrypted, key_l, alphabet, freqs=freqs, buffer_len=7)
     cracked_text = decrypt(encrypted, cracked_key, alphabet, freqs)
 
     pass
 
+def determinant_test():
+    # Tworzenie macierzy
+    A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+    # Wyświetlenie wyznacznika oryginalnej macierzy
+    print("Wyznacznik macierzy przed modyfikacją: ", np.linalg.det(A))
+
+    # Dodanie drugiego wiersza do pierwszego i trzeciego
+    A[0] = A[0] + A[1]
+    A[2] = A[2] + A[1]
+
+    # Wyświetlenie wyznacznika macierzy po dodaniu wierszy
+    print("Wyznacznik macierzy po dodaniu wierszy: ", np.linalg.det(A))
+
+    A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    # Zamiana drugiego i trzeciego wiersza miejscami
+    A[1], A[2] = A[2], A[1]
+
+    # Wyświetlenie wyznacznika macierzy po zamianie wierszy
+    print("Wyznacznik macierzy po zamianie wierszy: ", np.linalg.det(A))
+
+
+    A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    # Zamiana drugiej i trzeciej kolumny miejscami
+    A[:, 1], A[:, 2] = A[:, 2], A[:, 1].copy()
+
+    # Wyświetlenie wyznacznika macierzy po zamianie kolumn
+    print("Wyznacznik macierzy po zamianie kolumn: ", np.linalg.det(A))
+
+    A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    # Dodanie do pierwszego wiersza wartości drugiego wiersza pomnożonego przez 2
+    A[0] = A[0] + 2 * A[1]
+
+    # Wyświetlenie wyznacznika macierzy po dodaniu wiersza pomnożonego przez skalar
+    print("Wyznacznik macierzy po dodaniu wiersza pomnożonego przez skalar: ", np.linalg.det(A))
+
+    A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    A[2] += 2 * A[0]
+    print("Wyznacznik macierzy po dodanie do jednego wiersza innego wiersza przemnożonego przez stałą: ", np.linalg.det(A))
 
 if __name__ == '__main__':
     # swap_rows_test()
@@ -80,6 +122,7 @@ if __name__ == '__main__':
     # inv_key = invert_key(key, alphabet_len)
     # decrypted = encrypt(text, inv_key, alphabet, freqs)
 
-    crack_test()
+    # crack_test()
+    determinant_test()
 
     pass
