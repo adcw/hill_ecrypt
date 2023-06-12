431116a17ef82b6b1b65cf7bbc7c8456f03442ec
Przemek <rysiulaw@gmail.com>
2023-06-12 12:08:06 +0200

    final tests

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from string import ascii_uppercase as alphabet
 
-from pandas import read_csv
-
 import crack_cipher
 import hill_encrypt
 import hill_key
 import utils
-from crack_cipher import guess_key_len
 from hill_encrypt import encrypt
-from hill_encrypt import invert_key
-from hill_key import random_key
 from utils import preprocess_text
 
 """
@@ -19,7 +14,7 @@ Przemysław Kawa, Adrian Ćwiąkała
 Do łamania szyfru wykorzystano klasyczną metodę shotgun z kilkoma modyfikacjami
 - metoda wspinaczki generuje odwrotność klucza, więc wynikowy klucz jest jego odwrotnością.
 
-- wspinaczka realozowana jest na wielu procesach.
+- wspinaczka realizowana jest na wielu procesach.
 
 - procent zmiany klucza maleje wraz ze wzrostem jakości odszyfrowywanego nim tekstu, dzięki czemu na samym początku
   klucz jest zmieniany w stopniu znacznym, a pod koniec tylko nieznacznie.
@@ -27,6 +22,22 @@ Do łamania szyfru wykorzystano klasyczną metodę shotgun z kilkoma modyfikacja
 - Jeśli zbliżamy się do rozwiązania, uruchamiana jest funkcja oceniająca, które wiersze należy zmienić by złamać klucz.
   Metoda ta bazuje na obliczaniu jakości metodą bigram i ocenianiu które litery odszyfrowanego tekstu są już poprawne,
   a które należy dalej zmieniać. Zmieniane są tylko te wiersze, które generują litery nie pasujące do pozostałych.
+  
+- Wyniki testów manualnych na języku angielskim:
+    Minimalna długość tekstu przy kluczu 2x2 dla którego algorytm zawsze wykona pracę poniżej 30s to 290
+    Średni czas i procent udanych rozwiązań kiedy długość klucza jest nie znana i długość tekstu to 3336 
+    klucze nie rozwiązane w ciągu określonego czasu określiłem jako nie rozwiązane
+    key_len     |    mean_time    |   success rate  | min/max recorded | time_limit
+    2                3.35             100%           0.03/19.49          30s
+    3                165.6            98,5%          127.2/226.3         1 hour
+    4                720.4            82%            495.8/1312.3(21min) 1 hour    NOTE: jeśli nie uwzględnimy wynik 1312 to średnia wyjdzie 644.6
+    
+    NOTE for above ken_len 2: Warto zaznaczyć że proces zamykania i otwierania pod procesów trwa chwile przed i po zakończeniu
+    na moim komputerze zajmuje to w sumie około 20s (u kolegów zauważyłem inne czasy).
+    Dodatkowo metoda próbuje rozwiązać ken_len 2 za każdym razem (gdy długość klucza nie jest znana)
+    przez 30s powoduje to że wyniki są zawyżone w sumie około 50s+-10s.
+    Więc np wynik 132s ken_len 3 w teście, to 71s rozwiązywania samego klucza 3.
+    Platforma testowa to: amd ryzen 9 3900xt
 """
 
 
@@ -75,7 +86,7 @@ def test_crack(alph: str = alphabet,
                ngram_file_path: str = './language_data/english_trigrams.txt',
                letter_freqs_file_path: str = './language_data/english_letter_freqs.txt',
                text_to_crack_path: str = './english_text_to_crack.txt',
-               key_len2_timeout: int = 10,
+               key_len2_timeout: int = 30,
                crack_timeout: int = 4 * 60, target_score=-3.5, bad_score=-7):
     with open(text_to_crack_path, encoding="UTF-8") as file:
         text = file.read()
@@ -83,8 +94,9 @@ def test_crack(alph: str = alphabet,
     freqs = utils.parse_freqs(freqs_file_path=letter_freqs_file_path)
 
     text = preprocess_text(text, alphabet=alph)
-
-    key_len = 3
+    text = text[:280]
+    print(f"Text len: {len(text)}")
+    key_len = 2
     alphabet_len = len(alph)
     key = hill_key.random_key(key_len, alphabet_len)
 
@@ -97,28 +109,32 @@ def test_crack(alph: str = alphabet,
                                                    ngram_file_path=ngram_file_path,
                                                    freqs_file_path=freqs, target_score=target_score,
                                                    bad_score=bad_score,
-                                                   print_threshold=-100, key_len2_timeout=key_len2_timeout)
+                                                   print_threshold=-100,
+                                                   crack_timeout=60 * 60,
+                                                   key_len2_timeout=key_len2_timeout)
 
     with open("./output.txt", mode="w+", encoding="UTF-8") as file:
         file.write(cracked_text)
-    print(f"Cracked text was saved to the file.")
-
+    print(f"Cracked text was saved to the file: output.txt.")
 
 if __name__ == '__main__':
     # Łamanie tekstu angielskiego
-    # test_crack()
+    test_crack()
+    french_alphabet = utils.get_alphabet('language_data/english_alphabet.txt')
 
     # Łamanie tekstu polskiego
-    polish_alphabet = utils.get_alphabet('language_data/polish_alphabet.txt')
-    test_crack(alph=polish_alphabet,
-               bigram_file_path="language_data/polish_bigrams.txt",
-               ngram_file_path="language_data/polish_trigrams.txt",
-               letter_freqs_file_path="language_data/polish_letter_freqs.txt",
-               text_to_crack_path="polish_text_to_crack.txt",
-               key_len2_timeout=20,
-               bad_score=-7,
-               target_score=-4.8)
+    # polish_alphabet = utils.get_alphabet('language_data/polish_alphabet.txt')
+    # test_crack(alph=polish_alphabet,
+    #            bigram_file_path="language_data/polish_bigrams.txt",
+    #            ngram_file_path="language_data/polish_trigrams.txt",
+    #            letter_freqs_file_path="language_data/polish_letter_freqs.txt",
+    #            text_to_crack_path="polish_text_to_crack.txt",
+    #            key_len2_timeout=20,
+    #            bad_score=-7,
+    #            target_score=-4.8)
 
+    # utils.generate_grams("./language_data/french_text_sample.txt", "./language_data/french_bigrams.txt",
+    #                      "./language_data/french_alphabet.txt", 2)
     # Łamanie tekstu niemieciego
     # german_alphabet = utils.get_alphabet('language_data/german_alphabet.txt')
     # test_crack(alph=german_alphabet,
@@ -136,3 +152,25 @@ if __name__ == '__main__':
     #
     # utils.genereate_freqs("./language_data/polish_text_sample.txt", "./language_data/polish_letter_freqs.txt",
     #                       polish_alphabet)
+
+    # stress test
+    # it may take time so. Watch out
+    # array = []
+    # from time import time
+    #
+    # for i in range(0, 10):
+    #     ts = time()
+    #     test_crack()
+    #     array.append(time() - ts)
+    #     print(i, time() - ts)
+    # sum_value = sum(array)
+    # count = len(array)
+    # mean_value = sum_value / count  # Średnia jako iloraz sumy przez liczbę elementów
+    # print("max: ", max(array))
+    # print("min: ", min(array))
+    # count_below = sum(1 for value in array if value < 60 * 60)
+    # percentage_below = (count_below / len(array)) * 100
+    # print("success:", percentage_below)
+    # print("mean: ", mean_value)
+    # array = sorted(array, reverse=True)
+    # print(array)
